e551579ba441de1444346af4a7a330ea8ad1a48f
William Bryce Angell <devdecaf@gmail.com>
2024-07-12 09:26:12 -0500

    move path follow into mob

--- levels/level.gd
+++ levels/level.gd
@@ -6,14 +6,10 @@ extends Node
 func _ready():
 	$MobTimer.start(5)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$Path2D/PathFollow2D.progress_ratio += delta / 10
-
-
+	
 func _on_mob_timer_timeout():
 	var mob: Mob = mob_scene.instantiate()
 	
 	add_child(mob)
 	
-	mob.set_path_to_follow($Path2D/PathFollow2D)
+	mob.follow_path($Path2D)
--- mobs/mob.gd
+++ mobs/mob.gd
@@ -2,13 +2,19 @@ class_name Mob extends CharacterBody2D
 
 var path_to_follow: PathFollow2D
 
+
 func damage():
 	queue_free()
 
-func set_path_to_follow(path: PathFollow2D):
+
+func follow_path(path: Path2D):
 	if (!path_to_follow):
-		path_to_follow = path
+		path_to_follow = PathFollow2D.new()
+		path_to_follow.position = path.curve.get_point_position(0)
+		path.add_child(path_to_follow)
 		
-func _process(_delta):
+		
+func _process(delta):
+	path_to_follow.progress_ratio += delta / 10
 	global_position = path_to_follow.global_position
 	
