99c2381f30c8765c3b9293ef2b2c43e5c9381506
William Bryce Angell <devdecaf@gmail.com>
2024-07-26 09:13:05 -0500

    added ball animation and rotation towards target

--- projectiles/ball/ball.gd
+++ projectiles/ball/ball.gd
@@ -3,12 +3,20 @@ class_name Ball extends Area2D
 @export var target: Mob
 var damageAmount = 2
 var speed = 150
+var initial_rotation
+
+func _ready():
+	initial_rotation = rotation
+	_rotate_toward_target()
+	$AnimatedSprite2D.play("default")
+	
 
 func _process(delta):
 	if (target == null):
 		queue_free()
 		return
 	
+	_rotate_toward_target()
 	global_position = global_position.move_toward(target.global_position, delta * speed)
 	
 		
@@ -16,3 +24,10 @@ func _on_body_entered(mob):
 	if (target == mob):
 		queue_free()
 		target.damage(damageAmount)
+		
+		
+func _rotate_toward_target():
+	var angle = global_position.angle_to_point(target.global_position) + initial_rotation
+	print(angle)
+	rotation = angle
+	
